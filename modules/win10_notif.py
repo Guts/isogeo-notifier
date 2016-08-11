e84b6463eec7e5dff14af8b4d66585b202bb864b
Julien <julien.moura+dev@gmail.com>
2016-08-11 11:44:41 +0200

    PEP 8 and clean up

--- modules/win10_notif.py
+++ modules/win10_notif.py
@@ -6,18 +6,69 @@ import struct
 import time
 
 # Class
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from __future__ import (absolute_import, print_function, unicode_literals)
+
+# ##############################################################################
+# ########## Libraries #############
+# ##################################
+# standard library
+import logging
+from os import path
+from time import sleep
+
+# 3rd party modules
+from win32api import GetModuleHandle, PostQuitMessage
+from win32con import CW_USEDEFAULT, IMAGE_ICON, IDI_APPLICATION,\
+                     LR_DEFAULTSIZE, LR_LOADFROMFILE,\
+                     WM_DESTROY, WS_OVERLAPPED, WS_SYSMENU, WM_USER
+from win32gui import CreateWindow, DestroyWindow, LoadIcon, LoadImage,\
+                     NIF_ICON, NIF_INFO, NIF_MESSAGE, NIF_TIP,\
+                     NIM_ADD, NIM_DELETE, NIM_MODIFY,\
+                     RegisterClass, Shell_NotifyIcon, UpdateWindow, WNDCLASS
+
+# ############################################################################
+# ######### Main program ###########
+# ##################################
+
+
 class WindowsBalloonTip:
+    """Create a Windows 10 notification balloon.
+
+    from: https://github.com/jithurjacob/Windows-10-Toast-Notifications
+    """
+
     def __init__(self):
         message_map = { win32con.WM_DESTROY: self.OnDestroy,}
+        """Initialize."""
+        message_map = {WM_DESTROY: self.on_destroy, }
 
         # Register the window class.
         wc = WNDCLASS()
         self.hinst = wc.hInstance = GetModuleHandle(None)
         wc.lpszClassName = 'PythonTaskbar'
         wc.lpfnWndProc = message_map # could also specify a wndproc.
+        wc.lpszClassName = str("PythonTaskbar")  # must be a string
+        wc.lpfnWndProc = message_map  # could also specify a wndproc.
         self.classAtom = RegisterClass(wc)
 
                     
+    def balloon_tip(self, title="Notification", msg="Here comes the message",
+                    icon_path=None, duration=5):
+        """Notification settings.
+
+        :title: notification title
+        :msg: notification message
+        :icon_path: path to the .ico file to custom notification
+        :duration: delay in seconds before notification self-destruction
+        """
+        style = WS_OVERLAPPED | WS_SYSMENU
+        self.hwnd = CreateWindow(self.classAtom, "Taskbar", style,
+                                 0, 0, CW_USEDEFAULT,
+                                 CW_USEDEFAULT,
+                                 0, 0, self.hinst, None)
+        UpdateWindow(self.hwnd)
 
     def OnDestroy(self, hwnd, msg, wparam, lparam):
         nid = (self.hwnd, 0)
@@ -37,10 +88,15 @@ class WindowsBalloonTip:
         Shell_NotifyIcon(NIM_MODIFY, (hwnd, 0, NIF_INFO, win32con.WM_USER+20, hicon, 'Balloon Tooltip', msg, 200, title,NIIF_INFO))
         
         time.sleep(1)   
+        return None
 
     
 # Main
 if __name__ == '__main__':
+# ###############################################################################
+# ###### Stand alone program ########
+# ###################################
+if __name__ == "__main__":
     # Example
     w=WindowsBalloonTip()
     w.balloon_tip('Isogeo', '10 nouvelles data depuis la derniere execution')
