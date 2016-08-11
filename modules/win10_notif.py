c0b2b26a72fe67af39e2d24178e88912bab4ceeb
Julien <julien.moura+dev@gmail.com>
2016-08-11 11:44:59 +0200

    add possibility to custom icon and duration

--- modules/win10_notif.py
+++ modules/win10_notif.py
@@ -1,11 +1,3 @@
-from win32api import *
-from win32gui import *
-import win32con
-import sys, os
-import struct
-import time
-
-# Class
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from __future__ import (absolute_import, print_function, unicode_literals)
@@ -40,20 +32,16 @@ class WindowsBalloonTip:
     """
 
     def __init__(self):
-        message_map = { win32con.WM_DESTROY: self.OnDestroy,}
         """Initialize."""
         message_map = {WM_DESTROY: self.on_destroy, }
 
         # Register the window class.
         wc = WNDCLASS()
         self.hinst = wc.hInstance = GetModuleHandle(None)
-        wc.lpszClassName = 'PythonTaskbar'
-        wc.lpfnWndProc = message_map # could also specify a wndproc.
         wc.lpszClassName = str("PythonTaskbar")  # must be a string
         wc.lpfnWndProc = message_map  # could also specify a wndproc.
         self.classAtom = RegisterClass(wc)
 
-                    
     def balloon_tip(self, title="Notification", msg="Here comes the message",
                     icon_path=None, duration=5):
         """Notification settings.
@@ -70,34 +58,53 @@ class WindowsBalloonTip:
                                  0, 0, self.hinst, None)
         UpdateWindow(self.hwnd)
 
-    def OnDestroy(self, hwnd, msg, wparam, lparam):
+        # icon
+        icon_path = path.realpath(icon_path)
+        icon_flags = LR_LOADFROMFILE | LR_DEFAULTSIZE
+        try:
+            hicon = LoadImage(self.hinst, icon_path,
+                              IMAGE_ICON, 0, 0, icon_flags)
+        except Exception as e:
+            logging.error("Some trouble with the icon ({}): {}"
+                          .format(icon_path, e))
+            hicon = LoadIcon(0, IDI_APPLICATION)
+
+        # Taskbar icon
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+        nid = (self.hwnd, 0, flags, WM_USER + 20, hicon, "Tooltip")
+        Shell_NotifyIcon(NIM_ADD, nid)
+        Shell_NotifyIcon(NIM_MODIFY, (self.hwnd, 0, NIF_INFO,
+                                      WM_USER + 20,
+                                      hicon, "Balloon Tooltip", msg, 200,
+                                      title))
+        # take a rest then destroy
+        sleep(duration)
+        DestroyWindow(self.hwnd)
+        return None
+
+    def on_destroy(self, hwnd, msg, wparam, lparam):
+        """Clean after notification ended.
+
+        :hwnd:
+        :msg:
+        :wparam:
+        :lparam:
+        """
         nid = (self.hwnd, 0)
         Shell_NotifyIcon(NIM_DELETE, nid)
         PostQuitMessage(0)
 
-    def balloon_tip(self,title, msg):
-        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-        hwnd = CreateWindow(self.classAtom, "Taskbar",style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, self.hinst, None)
-        UpdateWindow(hwnd)
-
-        hicon = LoadIcon(0, win32con.IDI_APPLICATION)
-
-        flags =NIF_ICON | NIF_MESSAGE | NIF_TIP
-        nid = (hwnd, 0, flags, win32con.WM_USER+20, hicon, 'Tooltip')
-        Shell_NotifyIcon(NIM_ADD, nid)
-        Shell_NotifyIcon(NIM_MODIFY, (hwnd, 0, NIF_INFO, win32con.WM_USER+20, hicon, 'Balloon Tooltip', msg, 200, title,NIIF_INFO))
-        
-        time.sleep(1)   
         return None
 
-    
-# Main
-if __name__ == '__main__':
 # ###############################################################################
 # ###### Stand alone program ########
 # ###################################
 if __name__ == "__main__":
     # Example
-    w=WindowsBalloonTip()
-    w.balloon_tip('Isogeo', '10 nouvelles data depuis la derniere execution')
-    # w.balloon_tip('Example two', 'Once you start coding in Python you will hate other languages')
+    w = WindowsBalloonTip()
+    w.balloon_tip("Isogeo",
+                  "Restez informés sur l'état de vos partages",
+                  r"..\img\favicon.ico",
+                  3)
+    # w.balloon_tip("Example two",
+    #               "Once you start coding in Python you'll hate other languages")
